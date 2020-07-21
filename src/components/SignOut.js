07b6c37f64bf67584159e2a376c4bdcedb558994
Nicolas Demol <yolo@nicolass-macbook-pro.home>
2020-07-21 04:50:31 +0200

    v0.23c Leaderboard changed w/ history tab

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { authService } from "../services";
+import React from 'react';
+import { authService } from '../services';
 
 const SignOutButton = () => (
   <button
-    className="transition ease-in-out duration-300 bg-red-400 hover:bg-red-500 text-red-900 p-2 rounded"
+    className="transition ease-in-out duration-300 bg-red-400 hover:bg-red-500 text-red-900 p-2 font-bold rounded"
     type="button"
     onClick={authService.doSignOut}
   >
