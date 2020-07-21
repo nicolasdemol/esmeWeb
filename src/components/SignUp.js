f11c9cef2b79fa47599a19490ab2dfce08643a47
Nicolas Demol <yolo@nicolass-macbook-pro.home>
2020-07-21 18:20:15 +0200

    fix signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,116 +4,150 @@ import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../constants/routes';
+import { authService } from '../services';
 
-const SignUp = () => (
-        <SignUpForm />
-);
+const SignUp = () => <SignUpForm />;
 
 const INITIAL_STATE = {
-    firstname: '',
-    lastname: '',
-    email: '',
-    password: '',
-    error: null,
+  firstname: '',
+  lastname: '',
+  email: '',
+  password: '',
+  error: null
 };
 
 class SignUpFormBase extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { ...INITIAL_STATE };
-    }
-    
-    onSubmit = event => {
-        event.preventDefault()
-        
-        const { email, password } = this.state;
-        
-        this.props.firebase
-        .doCreateUserWithEmailAndPassword(email, password)
-        .then(authUser => {
-            this.setState({ ...INITIAL_STATE });
-            this.props.history.push(ROUTES.HOME);
-        })
-        .catch(error => {
-            this.setState({ error });
-        });
-        
-    };
+    this.state = { ...INITIAL_STATE };
+  }
 
-    onChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
-    };
+  onSubmit = (event) => {
+    event.preventDefault();
 
-    render() {
-        const {
-            firstname,
-            lastname,
-            email,
-            password,
-            error,
-        } = this.state;
+    const { email, password } = this.state;
 
-        let isInvalid = firstname === '' || lastname === '' || email === '' || password === '';
+    authService
+      .doCreateUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch((error) => {
+        this.setState({ error });
+      });
+  };
 
-        return (
-            <section className="h-screen flex justify-center bg-gray-200">
-                <div className="flex justify-center items-center my-20 sm:my-0">
-                    <div className="container max-w-md">
-                        <form onSubmit={this.onSubmit} className="w-full p-10 shadow-md bg-white px-8 pt-6 pb-8 mb-4">
-                            <div className="flex flex-wrap -mx-3 mb-6">
-                                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                <label className="block uppercase tracking-wide text-gray-600 text-xs font-bold mb-2" >
-                                    Prénom
-                                </label>
-                                <input name="firstname" value={firstname} onChange={this.onChange} className="shadow appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jean" />
-                                </div>
-                                <div className="w-full md:w-1/2 px-3">
-                                <label className="block uppercase tracking-wide text-gray-600 text-xs font-bold mb-2" >
-                                    Nom
-                                </label>
-                                <input name="lastname" value={lastname} onChange={this.onChange} className="shadow appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Dupont" />
-                                </div>
-                            </div>
-                            <div className="flex flex-wrap -mx-3 mb-6">
-                                <div className="w-full px-3">
-                                <label className="block uppercase tracking-wide text-gray-600 text-xs font-bold mb-2">
-                                    Email
-                                </label>
-                                <input name="email" value={email} onChange={this.onChange} className="shadow appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="text" placeholder="jean.dupont@gmail.com" />
-                                <p className="text-gray-600 text-xs italic">Votre adresse doit être valide.</p>
-                                </div>
-                            </div>
-                            <div className="flex flex-wrap -mx-3 mb-6">
-                                <div className="w-full px-3">
-                                <label className="block uppercase tracking-wide text-gray-600 text-xs font-bold mb-2">
-                                    Mot de passe
-                                </label>
-                                <input name="password" value={password} onChange={this.onChange} className="shadow appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-                                {error && <p className="text-red-500 text-xs italic">{error.message}</p>}
-                                </div>
-                            </div>
-                            <div className="flex items-center justify-center">
-                                    <button disabled={isInvalid} type="submit" className="transition ease-in-out duration-300 nav-links bg-teal-400 hover:bg-white border text-blue-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                                        Créer mon compte
-                                    </button>
-                            </div>
-                        </form>
-                        <p className="text-center text-gray-500 text-xs">
-                            &copy;2020 Sudrihack Corp. Tous droits réservés.
-                        </p>
-                    </div>
+  onChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { firstname, lastname, email, password, error } = this.state;
+
+    let isInvalid =
+      firstname === '' || lastname === '' || email === '' || password === '';
+
+    return (
+      <section className="h-screen flex justify-center bg-gray-200">
+        <div className="flex justify-center items-center my-20 sm:my-0">
+          <div className="container max-w-md">
+            <form
+              onSubmit={this.onSubmit}
+              className="w-full p-10 shadow-md bg-white px-8 pt-6 pb-8 mb-4"
+            >
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label className="block uppercase tracking-wide text-gray-600 text-xs font-bold mb-2">
+                    Prénom
+                  </label>
+                  <input
+                    name="firstname"
+                    value={firstname}
+                    onChange={this.onChange}
+                    className="shadow appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                    id="grid-first-name"
+                    type="text"
+                    placeholder="Jean"
+                  />
                 </div>
-            </section>
-            
-        );
-    }
+                <div className="w-full md:w-1/2 px-3">
+                  <label className="block uppercase tracking-wide text-gray-600 text-xs font-bold mb-2">
+                    Nom
+                  </label>
+                  <input
+                    name="lastname"
+                    value={lastname}
+                    onChange={this.onChange}
+                    className="shadow appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-last-name"
+                    type="text"
+                    placeholder="Dupont"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label className="block uppercase tracking-wide text-gray-600 text-xs font-bold mb-2">
+                    Email
+                  </label>
+                  <input
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    className="shadow appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-email"
+                    type="text"
+                    placeholder="jean.dupont@gmail.com"
+                  />
+                  <p className="text-gray-600 text-xs italic">
+                    Votre adresse doit être valide.
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label className="block uppercase tracking-wide text-gray-600 text-xs font-bold mb-2">
+                    Mot de passe
+                  </label>
+                  <input
+                    name="password"
+                    value={password}
+                    onChange={this.onChange}
+                    className="shadow appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-password"
+                    type="password"
+                    placeholder="******************"
+                  />
+                  {error && (
+                    <p className="text-red-500 text-xs italic">
+                      {error.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="flex items-center justify-center">
+                <button
+                  disabled={isInvalid}
+                  type="submit"
+                  className="transition ease-in-out duration-300 nav-links bg-teal-400 hover:bg-white border text-blue-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Créer mon compte
+                </button>
+              </div>
+            </form>
+            <p className="text-center text-gray-500 text-xs">
+              &copy;2020 Sudrihack Corp. Tous droits réservés.
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
-const SignUpForm = compose(
-    withRouter,
-    withFirebase,
-    )(SignUpFormBase); 
+const SignUpForm = compose(withRouter, withFirebase)(SignUpFormBase);
 
 export default SignUp;
 export { SignUpForm };
