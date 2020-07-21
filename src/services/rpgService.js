9f70bdb86af0f9dfe7ede32935b5219ec5654ed5
Leo <borniche.leo@gmail.com>
2020-07-21 16:42:31 +0200

    postContrib 2

--- src/services/rpgService.js
+++ src/services/rpgService.js
@@ -1,27 +1,35 @@
-import { getCollection, mapData } from "./utils";
-import { firestore } from "firebase"
+import { getCollection, mapData } from './utils';
+import { firestore } from 'firebase';
 
 export default class rpgService {
-    constructor(db){
-        this.db = db
-    }
+  constructor(db) {
+    this.db = db;
+  }
 
-    getActions = () => getCollection(this.db,"actions")
-    getCategories = () => getCollection(this.db,"categories")
-    getPointsRegistries = () => getCollection(this.db,"points_registry")
+  getActions = () => getCollection(this.db, 'actions');
+  getCategories = () => getCollection(this.db, 'categories');
+  getPointsRegistries = () => getCollection(this.db, 'points_registry');
 
-    async getContributions(uuid){
-        const qs = await this.db.collection("points_registry").doc(uuid).collection("contributions").get()
-        return mapData(qs);
-    }
-    async postContributions(uuid,{actionId,justification}){
+  async getContributions(uuid) {
+    const qs = await this.db
+      .collection('points_registry')
+      .doc(uuid)
+      .collection('contributions')
+      .get();
+    return mapData(qs);
+  }
 
-        const res = this.db.collection("points_registry").doc(uuid).collection("contributions").add({
-            action: this.db.doc(`actions/${actionId}`),
-            date: firestore.FieldValue.serverTimestamp(),
-            justification: justification
-        })
-        console.log(res)
-        return res;
-    }
+  async postContributions({ uuid, actionId, justification }) {
+    const res = this.db
+      .collection('points_registry')
+      .doc(uuid)
+      .collection('contributions')
+      .add({
+        action: this.db.doc(`actions/${actionId}`),
+        date: firestore.FieldValue.serverTimestamp(),
+        justification: justification
+      });
+    console.log(res);
+    return res;
+  }
 }
