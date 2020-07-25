7f2cc432b81e2f6091f6df788bf4476a19eb72a2
Nicolas Demol <yolo@Nicolass-MacBook-Pro.local>
2020-07-25 17:57:18 +0200

    delete Firebase folder create hook signin

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,12 +10,12 @@ const Menu = () => {
   };
 
   return (
-    <div class="relative inline-block text-left">
+    <div className="relative inline-block text-left">
       <div>
-        <span class="rounded-md shadow-sm">
+        <span className="rounded-md shadow-sm">
           <button
             type="button"
-            class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150"
+            className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150"
             id="options-menu"
             aria-haspopup="true"
             aria-expanded="true"
@@ -23,14 +23,14 @@ const Menu = () => {
           >
             Options
             <svg
-              class="-mr-1 ml-2 h-5 w-5"
+              className="-mr-1 ml-2 h-5 w-5"
               viewBox="0 0 20 20"
               fill="currentColor"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
           </button>
@@ -43,45 +43,46 @@ const Menu = () => {
 
 const DropdownMenu = () => {
   return (
-    <div class="origin-top-right relative md:absolute right-0 mt-4 w-56 rounded-md shadow-lg">
+    <div className="origin-top-right relative md:absolute right-0 mt-4 w-56 rounded-md shadow-lg">
       <div
-        class="rounded-md bg-white shadow-xs"
+        className="rounded-md bg-white shadow-xs"
         role="menu"
         aria-orientation="vertical"
         aria-labelledby="options-menu"
       >
-        <div class="py-1">
+        <div className="py-1">
           <NavLink
             to={ROUTES.ACCOUNT}
-            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+            className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+            role="menuitem"
           >
-            <button role="menuitem">Paramètres</button>
+            Paramètres
           </NavLink>
         </div>
-        <div class="border-t border-gray-100"></div>
-        <div class="py-1">
-          <button
-            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+        <div className="border-t border-gray-100"></div>
+        <div className="py-1">
+          <div
+            className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
             role="menuitem"
           >
             Partager
-          </button>
-          <button
-            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+          </div>
+          <div
+            className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
             role="menuitem"
           >
             Ajouter aux favoris
-          </button>
+          </div>
         </div>
-        <div class="border-t border-gray-100"></div>
-        <div class="py-1">
+        <div className="border-t border-gray-100"></div>
+        <div className="py-1">
           <NavLink
             to={ROUTES.LANDING}
-            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+            className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+            onClick={authService.doSignOut}
+            role="menuitem"
           >
-            <button onClick={authService.doSignOut} role="menuitem">
-              Se déconnecter
-            </button>
+            Se déconnecter
           </NavLink>
         </div>
       </div>
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,103 +4,100 @@ import { useHistory } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 import { authService } from '../services';
 
-const SignIn = () => (
-    <SignInForm />
-);
+const SignIn = () => <SignInForm />;
 
 const SignInForm = (props) => {
-  
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState(null)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
   let isInvalid = password === '' || email === '';
 
-  const history = useHistory()
+  const history = useHistory();
 
   const onSubmit = (event) => {
     event.preventDefault();
-    
+
     authService
       .doSignInWithEmailAndPassword(email, password)
       .then(() => {
-        setEmail('')
-        setPassword('')
-        setError(null)
-        history.push(ROUTES.HOME)
+        setEmail('');
+        setPassword('');
+        setError(null);
+        history.push(ROUTES.HOME);
       })
       .catch((error) => {
-        setError(error)
+        setError(error);
       });
   };
 
-  const onChange = (handler,e) => {
-    handler(e.target.value)
-  }
+  const onChange = (handler, event) => {
+    handler(event.target.value);
+  };
 
-    return (
-      <section className="h-screen bg-gray-200">
-        <div className="flex justify-center h-full items-center my-10 sm:my-0">
-          <div className=" container max-w-xs">
-            <form
-              onSubmit={onSubmit}
-              className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            >
-              <div className="mb-4">
-                <label className="block text-gray-600 text-sm font-bold mb-2">
-                  Email
-                </label>
-                <input
-                  name="email"
-                  value={email}
-                  onChange={e => onChange(setEmail,e)}
-                  className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="username"
-                  type="text"
-                  placeholder="Email"
-                />
-              </div>
-              <div className="mb-6">
-                <label className="block text-gray-600 text-sm font-bold mb-2">
-                  Mot de passe
-                </label>
-                <input
-                  name="password"
-                  value={password}
-                  onChange={e => onChange(setPassword,e)}
-                  className="shadow appearance-none border border rounded w-full py-3 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  id="password"
-                  type="password"
-                  placeholder="******************"
-                />
-                {error && (
-                  <p className="text-red-500 text-xs italic">{error.message}</p>
-                )}
-              </div>
-              <div className="flex items-center justify-between">
-                <button
-                  disabled={isInvalid}
-                  className="transition ease-in-out duration-300 nav-links bg-teal-400 hover:bg-white border text-blue-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="submit"
-                >
-                  Connection
-                </button>
-                <a
-                  className="inline-block px-2 align-baseline font-thin text-sm text-blue-800 hover:text-blue-600"
-                  href="http://sudrihack.com"
-                >
-                  Mot de passe oublié ?
-                </a>
-              </div>
-            </form>
-            <p className="text-center text-gray-500 text-xs">
-              &copy;2020 Sudrihack Corp. Tous droits réservés.
-            </p>
-          </div>
+  return (
+    <section className="h-screen bg-gray-200">
+      <div className="flex justify-center h-full items-center my-10 sm:my-0">
+        <div className=" container max-w-xs">
+          <form
+            onSubmit={onSubmit}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          >
+            <div className="mb-4">
+              <label className="block text-gray-600 text-sm font-bold mb-2">
+                Email
+              </label>
+              <input
+                name="email"
+                value={email}
+                onChange={(event) => onChange(setEmail, event)}
+                className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-600 text-sm font-bold mb-2">
+                Mot de passe
+              </label>
+              <input
+                name="password"
+                value={password}
+                onChange={(event) => onChange(setPassword, event)}
+                className="shadow appearance-none border border rounded w-full py-3 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+              {error && (
+                <p className="text-red-500 text-xs italic">{error.message}</p>
+              )}
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                disabled={isInvalid}
+                className="transition ease-in-out duration-300 nav-links bg-teal-400 hover:bg-gray-300 text-blue-800 font-bold py-2 px-6 rounded focus:outline-none"
+                type="submit"
+              >
+                Connection
+              </button>
+              <a
+                className="flex px-4 align-baseline font-thin text-sm text-blue-800 hover:text-blue-700"
+                href="http://sudrihack.com"
+              >
+                Mot de passe oublié ?
+              </a>
+            </div>
+          </form>
+          <p className="text-center text-gray-500 text-xs">
+            &copy;2020 Sudrihack Corp. Tous droits réservés.
+          </p>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
+};
 
 export default SignIn;
 
